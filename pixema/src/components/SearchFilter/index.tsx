b58983070c346b45e5c3293adddcd4ced7b2b8e7
Linchit21 <wladev.it@gmail.com>
2024-12-11 13:49:22 +0300

    fix: SearchFilter

--- pixema/src/components/SearchFilter/index.tsx
+++ pixema/src/components/SearchFilter/index.tsx
@@ -1,29 +1,56 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
 import styles from './index.module.scss';
+import { useState } from 'react';
 
 export function SearchFilter() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className={styles['search-filter']}>
       <div className={styles['search-filter__title']}>
         <div>Filters</div>
         <button type="button"></button>
       </div>
-      <form action="">
-        <div className={styles['search-filter__sort']}>
+
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles['search-filter__selectedOption']}>
           <div className={styles['search-filter__label']}>Sort by</div>
+
           <div className={styles['search-filter__toggle']}>
-            <div
-              className={`${styles['search-filter__radiobutton']} ${styles['search-filter__radiobutton_left']} `}
+            <input
+              type="radio"
+              id="RATING"
+              name="search-name"
+              value="RATING"
+              {...register('order')}
+            />
+            <label
+              className={`${styles['search-filter__radiobutton']} ${styles['search-filter__radiobutton_left']}`}
+              htmlFor="RATING"
             >
-              <input type="radio" id="RATING" name="sort" />
               Rating
-            </div>
-            <div
-              className={`${styles['search-filter__radiobutton']} ${styles['search-filter__radiobutton_right']} `}
+            </label>
+
+            <input
+              type="radio"
+              id="YEAR"
+              name="search-name"
+              value="YEAR"
+              {...register('order')}
+            />
+            <label
+              className={`${styles['search-filter__radiobutton']} ${styles['search-filter__radiobutton_right']}`}
+              htmlFor="YEAR"
             >
-              <input type="radio" id="YEAR" name="sort" />
               Year
-            </div>
+            </label>
           </div>
+
           <div className={styles['search-filter__line']}></div>
         </div>
 
@@ -37,29 +64,70 @@ export function SearchFilter() {
               type="text"
               name="search-name"
               id=""
+              {...register('keyword')}
             />
           </div>
+
           <div className={styles['search-filter__movie-name']}>
             <div className={styles['search-filter__label']}>Genre</div>
-            <input placeholder="" type="text" name="search-name" id="" />
+            <input
+              placeholder=""
+              type="number"
+              name="search-name"
+              id=""
+              {...register('genres')}
+            />
           </div>
+
           <div className={styles['search-filter__years']}>
             <div className={styles['search-filter__label']}>Years</div>
             <div>
-              <input placeholder="From" type="text" name="search-name" id="" />
-              <input placeholder="To" type="text" name="search-name" id="" />
+              <input
+                placeholder="From"
+                type="number"
+                name="search-name"
+                id=""
+                {...register('yearFrom')}
+              />
+              <input
+                placeholder="To"
+                type="number"
+                name="search-name"
+                id=""
+                {...register('yearTo')}
+              />
             </div>
           </div>
+
           <div className={styles['search-filter__years']}>
             <div className={styles['search-filter__label']}>Rating</div>
             <div>
-              <input placeholder="From" type="text" name="search-name" id="" />
-              <input placeholder="To" type="text" name="search-name" id="" />
+              <input
+                placeholder="From"
+                type="number"
+                name="search-name"
+                id=""
+                {...register('ratingFrom')}
+              />
+              <input
+                placeholder="To"
+                type="number"
+                name="search-name"
+                id=""
+                {...register('ratingTo')}
+              />
             </div>
           </div>
+
           <div className={styles['search-filter__countries']}>
             <div className={styles['search-filter__label']}>Countries</div>
-            <select name="" id=""></select>
+            <select name="" id="">
+              <option value=""></option>
+              <option value="">1</option>
+              <option value="">2</option>
+              <option value="">3</option>
+              <option value="">4</option>
+            </select>
           </div>
         </div>
 
@@ -72,7 +140,7 @@ export function SearchFilter() {
           </button>
           <button
             className={`${styles['search-filter__button']} ${styles['search-filter__button-submit']} `}
-            type="button"
+            type="submit"
           >
             Show results
           </button>
