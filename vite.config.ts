1c3ead16774f20301d56a6910f32db10c10ec3cd
Linchit21 <wladev.it@gmail.com>
2025-11-14 20:10:10 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+   base: '/pixema_diplom/',
   plugins: [react()],
   optimizeDeps: {
     include: ['swiper/react', 'swiper/modules'],
